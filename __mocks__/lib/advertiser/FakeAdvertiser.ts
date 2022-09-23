384dff797034dcb11bb6cd9efdad4255f1fab22c
Fernandicus <80597450+Fernandicus@users.noreply.github.com>
2022-09-23 16:04:18 +0200

    atdd: advertiser/create in green

--- __mocks__/lib/advertiser/FakeAdvertiser.ts
+++ __mocks__/lib/advertiser/FakeAdvertiser.ts
@@ -1,2 +1,64 @@
+import {
+  Advertiser,
+  AdvertiserProps,
+  AdvertiserPropsPrimitives,
+} from "@/src/advertiser/domain/Advertiser";
+import { AdvertiserId } from "@/src/advertiser/domain/value-objects/AdvertiserId";
+import {
+  AdvertiserRol,
+  AdvertiserRolType,
+} from "@/src/advertiser/domain/value-objects/AdvertiserRol";
+import { Email } from "@/src/domain/Email";
+import { Name } from "@/src/domain/Name";
+import { Password } from "@/src/domain/Password";
+import { UniqId } from "@/src/utils/UniqId";
+import { faker } from "@faker-js/faker";
 
-export class FakeAdvertiser{}
+export class FakeAdvertiser extends Advertiser {
+  constructor(props: AdvertiserProps) {
+    super(props);
+  }
+
+  static create(rol: AdvertiserRolType): FakeAdvertiser {
+    const primaryData = this.generateFakeData();
+    const advertiser = this.generateAdvertiser({
+      ...primaryData,
+      rol,
+    });
+    return new FakeAdvertiser({ ...advertiser });
+  }
+
+  static createPrimitives(rol: AdvertiserRolType): AdvertiserPropsPrimitives {
+    const primaryData = this.generateFakeData();
+    return {
+      ...primaryData,
+      rol,
+    };
+  }
+
+  private static generateFakeData(): {
+    email: string;
+    name: string;
+    password: string;
+    id: string;
+  } {
+    return {
+      email: faker.company.name(),
+      name: faker.internet.email(),
+      password: faker.internet.password(),
+      id: UniqId.generate(),
+    };
+  }
+
+  private static generateAdvertiser(
+    primitives: AdvertiserPropsPrimitives
+  ): Advertiser {
+    return new Advertiser({
+      id: new AdvertiserId(primitives.id),
+      email: new Email(primitives.email),
+      name: new Name(primitives.name),
+      password: new Password(primitives.password),
+      rol: new AdvertiserRol(primitives.rol),
+    });
+  }
+}
--- __tests__/pages/api/advertiser/create/index.test.ts
+++ __tests__/pages/api/advertiser/create/index.test.ts
@@ -1,25 +1,65 @@
 import createAdvertiser from "@/pages/api/advertiser/create/index";
-import { AdvertiserPropsPrimitives } from "@/src/advertiser/domain/Advertiser";
+import {
+  Advertiser,
+  AdvertiserPropsPrimitives,
+} from "@/src/advertiser/domain/Advertiser";
+import { AdvertiserRolType } from "@/src/advertiser/domain/value-objects/AdvertiserRol";
 import { NextApiRequest, NextApiResponse } from "next";
 import httpMock, { MockRequest, MockResponse } from "node-mocks-http";
+import { FakeAdvertiser } from "../../../../../__mocks__/lib/advertiser/FakeAdvertiser";
 
-describe("On api/advertiser/create, GIVEN ....", () => {
-  it("WHEN . . . ", async () => {
-    const body: AdvertiserPropsPrimitives = {
-      id: "string",
-      name: "string",
-      email: "string",
-      password: "string",
-      rol: "agency",
-    };
+describe("On api/advertiser/create, GIVEN an Advertiser ", () => {
+  let advertiser: AdvertiserPropsPrimitives;
 
+  beforeAll(() => {
+    advertiser = FakeAdvertiser.createPrimitives(AdvertiserRolType.BUSINESS);
+  });
+
+  it(`WHEN send a POST request with advertiser data,
+  THEN response status code should be 200`, async () => {
     let req: MockRequest<NextApiRequest> = httpMock.createRequest({
       method: "POST",
-      body,
+      body: advertiser,
     });
     let res: MockResponse<NextApiResponse> = httpMock.createResponse();
 
     await createAdvertiser(req, res);
-    expect(res.statusCode).toBe(200)
+    expect(res.statusCode).toBe(200);
+  });
+
+  it(`WHEN send a GET request with advertiser data,
+  THEN response status code should be 400`, async () => {
+    let req: MockRequest<NextApiRequest> = httpMock.createRequest({
+      method: "GET",
+      body: advertiser,
+    });
+    let res: MockResponse<NextApiResponse> = httpMock.createResponse();
+
+    await createAdvertiser(req, res);
+    expect(res.statusCode).toBe(400);
+  });
+
+  it(`WHEN send a POST request with empty advertiser data,
+  THEN response status code should be 400`, async () => {
+    let req: MockRequest<NextApiRequest> = httpMock.createRequest({
+      method: "POST",
+      body: {},
+    });
+    let res: MockResponse<NextApiResponse> = httpMock.createResponse();
+
+    await createAdvertiser(req, res);
+    expect(res.statusCode).toBe(400);
+  });
+
+  it(`WHEN send a POST request with an advertiser rol of user,
+  THEN response status code should be 400`, async () => {
+    let req: MockRequest<NextApiRequest> = httpMock.createRequest({
+      method: "POST",
+      body: {...advertiser, rol: "user"},
+    });
+    let res: MockResponse<NextApiResponse> = httpMock.createResponse();
+
+    await createAdvertiser(req, res);
+    expect(res.statusCode).toBe(400);
   });
 });
