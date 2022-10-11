dd3fe66015902bc21a883aaca12a17b01f48729c
Fernandicus <80597450+Fernandicus@users.noreply.github.com>
2022-10-11 12:14:05 +0200

    tdd find advertiser

--- src/advertiser/domain/AdvertiserRepo.ts
+++ src/advertiser/domain/AdvertiserRepo.ts
@@ -3,5 +3,6 @@ import { AdvertiserPropsPrimitives } from "./Advertiser";
 export interface AdvertiserRepo {
   save(model: AdvertiserPropsPrimitives): Promise<void>;
   findById(id: string): Promise<AdvertiserPropsPrimitives | null>;
+  findByEmail(email:string): Promise<AdvertiserPropsPrimitives | null>;
   //remove(id: string): Promise<void>;
 }
