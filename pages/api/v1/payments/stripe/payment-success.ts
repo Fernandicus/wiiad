f2f4fe1b0c7a262d57ca76a95686572de32a4d0a
Fernandicus <80597450+Fernandicus@users.noreply.github.com>
2022-11-21 17:01:23 +0100

    removed logs

--- pages/api/v1/payments/stripe/payment-success.ts
+++ pages/api/v1/payments/stripe/payment-success.ts
@@ -37,10 +37,7 @@ export default async function handler(
   let object: { metadata: IStripeMetadata; amount: number };
 
   try {
-    console.log("BODY : ");
-    console.log(req.body);
-    event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
-    console.log("EVENT TYPE: ", event.type);
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
     object = event.data.object as { metadata: IStripeMetadata; amount: number };
   } catch (err) {
     console.error(err);
