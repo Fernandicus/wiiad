be85c0ccd54fa0f42d32884dfe09e58a6069d59e
Fernando G <fernanprojects@gmail.com>
2023-03-03 18:01:42 +0100

    Fixed bug in FindReferral test

--- __tests__/src/referral/use-case/FindReferral.test.ts
+++ __tests__/src/referral/use-case/FindReferral.test.ts
@@ -24,12 +24,22 @@ describe(`On FindReferral, GIVEN a User id and some Referrals in MongoDB`, () =>
   it(`WHEN call findByUserId, THEN repo findByUserID should be called with user id`, async () => {
     const referralFound = await findReferral.findByUserId(user.id);
     expect(mockedRepo.findByUserID).toBeCalledWith(user.id);
-    expect(referralFound.userId).toEqual(user.id);
+
+    referralFound.match({
+      nothing() {},
+      some(value) {
+        expect(value.userId).toEqual(user.id);
+      },
+    });
   });
 
   it(`WHEN call findByUserId for a non existing userId, THEN an ErrorFindingReferral should be thrown`, async () => {
-    expect(findReferral.findByUserId(UniqId.new())).rejects.toThrowError(
-      ErrorFindingReferral
-    );
+    const referralFound = await findReferral.findByUserId(UniqId.new());
+    referralFound.match({
+      nothing() {
+        expect(null).toBeNull();
+      },
+      some(value) {},
+    });
   });
 });
