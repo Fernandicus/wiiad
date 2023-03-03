f49f444808e1b611cd2b5f45f1d5c60fc6bed5dd
Fernando G <fernanprojects@gmail.com>
2023-03-03 18:54:41 +0100

    Added missing logic in remove ad tests

--- __tests__/pages/api/ads/remove/index.test.ts
+++ __tests__/pages/api/ads/remove/index.test.ts
@@ -6,32 +6,39 @@ import { FakeAd } from "../../../../../__mocks__/lib/modules/ads/FakeAd";
 import { mockedContext } from "../../../../../__mocks__/context/MockContext";
 import { FakeAdvertiser } from "../../../../../__mocks__/lib/modules/user/FakeAdvertiser";
 import { userSession } from "@/src/modules/session/infrastructure/session-container";
-import { setTestAdDB } from "../../../../../__mocks__/lib/infrastructure/db/TestAdDB";
+import { setTestAdDB, TestAdDB } from "../../../../../__mocks__/lib/infrastructure/db/TestAdDB";
 import { IUserPrimitives } from "@/src/modules/users/user/domain/User";
+import { setTestCampaignDB } from "../../../../../__mocks__/lib/infrastructure/db/TestCampaignDB";
+import { Ad } from "@/src/modules/ad/domain/Ad";
+import { TestCampaignDB } from "../../../../../__mocks__/lib/infrastructure/db/TestCampaignDB";
 
 describe("On api/ads/remove-ad route", () => {
   let advertiser: IUserPrimitives;
+  let adWithActiveCampaign: Ad;
+  let adsDB: TestAdDB;
+  let campaignsDB: TestCampaignDB;
 
   beforeEach(async () => {
-    const advertisers = FakeAdvertiser.createMany(5);
-    await setTestAdDB(advertisers);
+    const advertisers = FakeAdvertiser.createMany(9);
+    adsDB = await setTestAdDB(advertisers);
+    const ads = await adsDB.getAllAds();
+    adWithActiveCampaign = ads[0];
+    campaignsDB = await setTestCampaignDB({
+      activeCampaignAds: ads.slice(0, 2),
+      standByCampaignAds: ads.slice(3, 5),
+      finishedCampaignAds: ads.slice(6, 8),
+    });
     advertiser = advertisers[0].toPrimitives();
   }, 8000);
 
-  it("WHEN send 'DELETE' method with correct ad id and a user session, THEN response with status 200", async () => {
-    const adData = FakeAd.createWithPrimitives(advertiser.id);
-
-    const ad = new AdModel({
-      _id: adData.id,
-      ...adData,
-    });
-
-    await ad.save();
+  it(`WHEN send 'DELETE' method with correct ad id and a user session, 
+  THEN response with status 200 and the campaign should be removed`, async () => {
+    const adData = adWithActiveCampaign;
 
     const ctx = mockedContext({
       method: "DELETE",
       body: {
-        adId: ad.id,
+        adId: adData.toPrimitives().id,
       },
     });
 
@@ -39,7 +46,14 @@ describe("On api/ads/remove-ad route", () => {
 
     await removeAd(ctx.req, ctx.res);
 
+    const adsFound = await adsDB.getAllAds();
+    const adFound = adsFound.find(ad=> ad.id.id === adData.id.id)
+    const campaignFound = await campaignsDB.findById(adData.id)
+
     expect(ctx.res.statusCode).toBe(200);
+    expect(campaignFound).toBeNull();
+    expect(campaignFound).toBeNull();
+    expect(adFound).toBeUndefined();
   }, 8000);
 
   it("WHEN send 'DELETE' method with correct ad id but without session, THEN response with status 400", async () => {
