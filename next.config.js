7073f5f32a518b25e928486d6b2a71cd9b4b1d62
Fernando G <fernanprojects@gmail.com>
2023-03-10 21:24:10 +0100

    added next config to try aws amplify

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.expoprts = {
+  env: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SMTP_SERVER: process.env.SMTP_SERVER,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    SMTP_PORT: process.env.MY_SMTP_PORT,
+    EMAIL_FROM: fprocess.env.EMAIL_FROM,
+  },
+};
