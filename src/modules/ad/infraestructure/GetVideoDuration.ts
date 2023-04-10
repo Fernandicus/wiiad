bb55f9320dc0a53e5512e0a4d1b53da19a80aecc
Fernando G <fernanprojects@gmail.com>
2023-04-10 14:05:24 +0200

    replaced get video duration package by ffprobe

--- src/modules/ad/infraestructure/GetVideoDuration.ts
+++ src/modules/ad/infraestructure/GetVideoDuration.ts
@@ -1,12 +1,14 @@
-import getVideoDurationInSeconds from "get-video-duration";
 import { IGetAdDuration } from "../domain/interfaces/IGetAdDuration";
 import { AdDuration } from "../domain/value-objects/AdDuration";
 import { AdFileUrl } from "../domain/value-objects/AdFileUrl";
+import ffprobe from "ffprobe";
+import ffprobeStatic from  "ffprobe-static";
 
 export class GetVideoDuration implements IGetAdDuration {
   async get(adFile: AdFileUrl): Promise<AdDuration> {
     if (adFile.isVideo()) {
-      const duration = await getVideoDurationInSeconds(adFile.file);
+      const videoInfo = await ffprobe(adFile.file, { path: ffprobeStatic.path });
+      const duration = videoInfo.streams[0].duration!
       return new AdDuration(duration);
     }
     return AdDuration.defaultTime();
