880ac5d07b026e866861f4ea825a1524886ed3ed
Fernando G <fernanprojects@gmail.com>
2023-04-12 18:08:26 +0200

    removed nav bar in sign in page

--- components/ui/pages/sign-in/SignInPage.tsx
+++ components/ui/pages/sign-in/SignInPage.tsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 import { ScreenPageLayout } from "../../layouts/ScreenPageLayout";
 import { SignInCard } from "../../forms/sign-in/SignInCard";
 import { TertiaryButton } from "../../buttons/TertiaryButton";
+import { Notification } from "../../notifications/Notification";
 
-export const SignInPage = ()=>{
-    const [userRole, setRole] = useState<RoleType>(RoleType.USER);
+export const SignInPage = () => {
+  const [userRole, setRole] = useState<RoleType>(RoleType.USER);
   const { setNotification } = useNotification();
 
   const isUserRole = userRole === RoleType.USER;
@@ -14,35 +15,40 @@ export const SignInPage = ()=>{
     ? "Quiero anunciarme!"
     : `Soy influencer!`;
 
-    return <ScreenPageLayout>
-    <div className="space-y-20 w-full max-w-lg mx-auto">
-      <SignInCard
-        logIn
-        userRole={userRole}
-        onErrorSubmit={(err) => {
-          setNotification({
-            message: err.message as string,
-            status: "error",
-          });
-        }}
-        onSuccessSubmit={() => {
-          setNotification({
-            message: "Te hemos enviado un email",
-            status: "success",
-          });
-        }}
-      />
-      <div className="space-x-2 w-full text-center">
-        <TertiaryButton
-          type="button"
-          onClick={() => {
-            if (!isUserRole) setRole(RoleType.USER);
-            else setRole(RoleType.BUSINESS);
-          }}
-        >
-          {tertiaryButtonLabel}
-        </TertiaryButton>
-      </div>
+  return (
+    <div className="bg-slate-100 max-h-full min-h-screen w-full">
+        <section className="min-h-screen flex justify-center items-center ">
+          <div className=" space-y-5 w-full max-w-lg mx-auto pt-5">
+            <SignInCard
+              logIn
+              userRole={userRole}
+              onErrorSubmit={(err) => {
+                setNotification({
+                  message: err.message as string,
+                  status: "error",
+                });
+              }}
+              onSuccessSubmit={() => {
+                setNotification({
+                  message: "Te hemos enviado un email",
+                  status: "success",
+                });
+              }}
+            />
+            <div className="space-x-2 w-full text-center">
+              <TertiaryButton
+                type="button"
+                onClick={() => {
+                  if (!isUserRole) setRole(RoleType.USER);
+                  else setRole(RoleType.BUSINESS);
+                }}
+              >
+                {tertiaryButtonLabel}
+              </TertiaryButton>
+            </div>
+          </div>
+        </section>
+      <Notification />
     </div>
-</ScreenPageLayout>
-}
+  );
+};
